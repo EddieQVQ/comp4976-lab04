3bc24f31cbb0ec6d7c6851978a161821907d354c
EddieQVQ <wxu55@my.bcit.ca>
2025-10-01 20:37:24 -0700

    Initial commit: MCP Server with Entity Framework and secure configuration

---
+++ ServerMCP/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using ServerMCP.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMcpServer()
+.WithHttpTransport()
+.WithToolsFromAssembly();
+
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    option => option.UseSqlite(connStr)
+);
+
+var app = builder.Build();
+
+app.MapMcp();
+
+
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
+
+app.Run();
